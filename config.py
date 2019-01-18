adc684b5b3b8593e4e81bbf40a0fc2007a10c5e2
Andrei Dziaineka <dziaineka@gmail.com>
2019-01-18 21:12:33 +0300

    available letter languages

--- config.py
+++ config.py
@@ -18,3 +18,7 @@ ADMIN_ID = 00000000
 # yandex maps
 YANDEX_MAPS_API_KEY = 'PUT_TOKEN_HERE'
 BASE_YANDEX_MAPS_URL = 'https://geocode-maps.yandex.ru/1.x/'
+
+# letter language
+BY = '_by'
+RU = '_ru'
