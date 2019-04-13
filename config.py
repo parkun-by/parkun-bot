150466d6f1543c34648a3e30e09b2e82fdce5b89
Andrei Dziaineka <dziaineka@gmail.com>
2019-04-13 13:17:22 +0300

    max twitter objects constants

--- config.py
+++ config.py
@@ -63,3 +63,5 @@ CONSUMER_KEY = 'consumer_key'
 CONSUMER_SECRET = 'consumer_secret'
 ACCESS_TOKEN = 'access_token'
 ACCESS_TOKEN_SECRET = 'access_token_secret'
+MAX_TWI_CHARACTERS = 280
+MAX_TWI_PHOTOS = 4
