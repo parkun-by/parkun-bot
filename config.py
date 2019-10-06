840918ddb464b2200c599f28074707fe6c43aec2
Andrei Dziaineka <dziaineka@gmail.com>
2019-10-06 22:47:21 +0300

    dev config template

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
-# bot
+# telegram bot token. Get it here https://t.me/BotFather
 API_TOKEN = 'PUT_TOKEN_HERE'
 URL_BASE = 'https://api.telegram.org/file/bot' + API_TOKEN + '/'
 
+# violation photos count upper bound in single appeal
 MAX_VIOLATION_PHOTOS = 10
 
-# letter language
+# appeal language
 BY = '_by'
 RU = '_ru'
 
@@ -31,36 +32,36 @@ REGIONS = [
 ]
 
 EMAIL_TO = {
-    MINSK: 'pismo_guvd_minsk@mia.by',
-    BREST_REGION: 'uvdbrest@brest.by',
-    VITSEBSK_REGION: 'UVD_Vitebsk@mvd.gov.by',
-    HOMEL_REGION: 'uvd_gomel@mvd.gov.by',
-    HRODNA_REGION: 'UVD_Grodno@mvd.gov.by',
-    MINSK_REGION: 'pismo_guvd_minsk@mia.by',
-    MAHILEU_REGION: 'uvd@mogilev.by',
+    MINSK: 'PUT_YOUR_TEST_EMAIL',
+    BREST_REGION: 'PUT_YOUR_TEST_EMAIL',
+    VITSEBSK_REGION: 'PUT_YOUR_TEST_EMAIL',
+    HOMEL_REGION: 'PUT_YOUR_TEST_EMAIL',
+    HRODNA_REGION: 'PUT_YOUR_TEST_EMAIL',
+    MINSK_REGION: 'PUT_YOUR_TEST_EMAIL',
+    MAHILEU_REGION: 'PUT_YOUR_TEST_EMAIL',
 }
 
 # redis
 REDIS_HOST = 'localhost'
-REDIS_PORT = '6379'
+REDIS_PORT = '16379'
 REDIS_PASSWORD = ''
 
-# admin
+# bot owner's telegram id to receive feedback
 ADMIN_ID = 00000000
 
 # yandex maps
-YANDEX_MAPS_API_KEY = 'PUT_TOKEN_HERE'
-BASE_YANDEX_MAPS_URL = 'https://geocode-maps.yandex.ru/1.x/?'
+YANDEX_MAPS_API_KEY = 'UNNECESSARY_FOR_DEV'
+BASE_YANDEX_MAPS_URL = 'http://localhost:18080/yandex_maps/?'
 ADDRESS_FAIL = 'no_address'
 
-# channel
+# to post into channel bot needs to be admin there
 CHANNEL = '@channel_name'
 
-# verifier
-MAIL_VERIFIER_URL = 'PUT_VERIFIER_URL'
+# email verifier url
+MAIL_VERIFIER_URL = 'http://localhost:18080/validate'  # response 111
 VERIFYING_FAIL = '42'
 
-# Twitter
+# Twitter twitter unnecessary for general development
 CONSUMER_KEY = 'consumer_key'
 CONSUMER_SECRET = 'consumer_secret'
 ACCESS_TOKEN = 'access_token'
