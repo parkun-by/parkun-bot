c717244b0b07ef55665476a66554690359a6d6ad
Andrei Dziaineka <dziaineka@gmail.com>
2019-10-23 23:03:29 +0300

    sharing and text to user

--- broadcaster.py
+++ broadcaster.py
@@ -5,5 +5,16 @@ class Broadcaster:
     def __init__(self, getter, locales):
         self.twitter = Twitter(getter, locales)
 
-    async def share(self, data):
-        await self.twitter.post(data)
+    async def share(self,
+                    language: str,
+                    file_paths: list,
+                    coordinates: list,
+                    date_time: str,
+                    plate: str,
+                    address: str) -> None:
+        await self.twitter.post(language,
+                                file_paths,
+                                coordinates,
+                                date_time,
+                                plate,
+                                address)
