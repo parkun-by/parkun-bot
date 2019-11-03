b14100a02102da9e6ba3db5ce8713e2c00bdf732
Andrei Dziaineka <dziaineka@gmail.com>
2019-11-03 12:58:32 +0300

    entering captcha timer

---
+++ timer.py
@@ -0,0 +1,41 @@
+import asyncio
+from datetime import datetime, timedelta
+from typing import Awaitable
+
+
+class Timer:
+    def __init__(self, stop_callback):
+        self.tasks_to_stop = {}
+        self.stop_callback = stop_callback
+
+    async def start(self):
+        while True:
+            await self._check_for_overdue()
+            await asyncio.sleep(60)
+
+    async def _check_for_overdue(self):
+        delete_list = []
+
+        for task in self.tasks_to_stop:
+            if datetime.utcnow() >= self.tasks_to_stop[task]['stop_time']:
+                await self.stop_callback(
+                    self.tasks_to_stop[task]['description'])
+
+                delete_list.append(task)
+
+        for task in delete_list:
+            self.tasks_to_stop.pop(task, '')
+
+    def add_task(self, task_description: dict, timer_min: float) -> None:
+        user_id = str(task_description["user_id"])
+        appeal_id = str(task_description["appeal_id"])
+        key = f'{user_id}_{appeal_id}'
+
+        self.tasks_to_stop[key] = {
+            'description': task_description,
+            'stop_time': datetime.utcnow() + timedelta(minutes=timer_min)
+        }
+
+    def delete_task(self, user_id: int, appeal_id: int) -> None:
+        key = f'{str(user_id)}_{str(appeal_id)}'
+        self.tasks_to_stop.pop(key, '')
