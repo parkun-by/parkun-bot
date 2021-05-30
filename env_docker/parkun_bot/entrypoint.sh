3ce62718eae39cdaf731c7555fe73c69719e2e50
Andrei Dziaineka <a_dziaineka@wargaming.net>
2021-05-30 18:51:17 +0300

    revert copying config

--- env_docker/parkun_bot/entrypoint.sh
+++ env_docker/parkun_bot/entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 # Docker entrypoint script.
 
+# copy config from volume mounted in /tmp/config/
+CONFIG=/tmp/parkun_config/.env
+if [ -f "$CONFIG" ]; then
+    echo "$CONFIG exist"
+    cp -a $CONFIG ./
+fi
+
 python ./main.py
